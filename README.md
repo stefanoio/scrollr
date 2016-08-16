3dc6f40d5a96ce18a7a5e5e4c5a45db6f39a8467
Stefano.io <hello@stefano.io>
2016-08-16 18:44:22 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Scrollr
+## Easy peasy smooth scrolling.
+
+Scrollr is a small (<1KB) utility that smooths #anchor scrolling inside a page. Just drop the script in your page, no need for configuration or initialization or markup changes.
+It works with links, js changes to location, manual changes to #hash by the user, and history too.
+
+It works on any modern browser (IE10+) and has no dependency from any other libraries.
+
+Here’s a demo: http://stefano.io/scrollr/demo.html
+
+To build a minified version inside the dist folder you can use:
+```
+npm install && npm run build
+```
---
+++ src/scrollr.js
@@ -0,0 +1,89 @@
+/*!
+ * scrollr
+ *
+ * Stefano Peloso - https://github.com/stefanoio/scrollr
+ *
+ * Free to use under terms of WTFPL version 2 license
+ */
+
+(function() {
+	"use strict";
+
+	var
+		DURATION = 300,
+
+		hasPopped = false,
+		lastPageYOffset = pageYOffset,
+		secondLastPageYOffset = pageYOffset,
+		targetPageYOffset,
+		speed,
+		lastT,
+
+		animate = function(t) {
+			var
+				newPageYOffset = Math.round(pageYOffset + (t - lastT) * speed);
+			if(((speed > 0) && (newPageYOffset >= targetPageYOffset)) || ((speed < 0) && (newPageYOffset <= targetPageYOffset))) {
+				lastPageYOffset = secondLastPageYOffset = pageYOffset;
+				scrollTo(pageXOffset, targetPageYOffset);
+				window.addEventListener("scroll", scroll, false);
+			} else {
+				lastT = t;
+				scrollTo(pageXOffset, newPageYOffset);
+				if(newPageYOffset === pageYOffset) {
+					requestAnimationFrame(animate);
+				}
+			}
+		},
+
+		popstate = function() {
+			var
+				target;
+			scrollTo(pageXOffset, lastPageYOffset);
+			targetPageYOffset = false;
+			if((location.hash === "#") || (location.href.substr(-1) === "#")) {
+				targetPageYOffset = 0;
+			} else {
+				target = document.getElementById(location.hash.substr(1));
+				if(target) {
+					targetPageYOffset = target.getBoundingClientRect().top + lastPageYOffset;
+				}
+			}
+			if(targetPageYOffset !== false) {
+				speed = (targetPageYOffset - pageYOffset) / DURATION;
+				hasPopped = true;
+				window.removeEventListener("scroll", scroll, false);
+				requestAnimationFrame(function(t) {
+					lastT = t;
+					scrollTo(pageXOffset, lastPageYOffset);
+					requestAnimationFrame(animate);
+				});
+			}
+		},
+
+		hashchange = function() { // workaround for this IE/Egde bug: https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/3740423/
+			if(hasPopped) {
+				hasPopped = false;
+			} else {
+				lastPageYOffset = secondLastPageYOffset;
+				popstate();
+			}
+		},
+
+		scroll = function() {
+			secondLastPageYOffset = lastPageYOffset;
+			lastPageYOffset = pageYOffset;
+		},
+
+		init = function() {
+			if("requestAnimationFrame" in window) {
+				window.addEventListener("scroll", scroll, false);
+				window.addEventListener("popstate", popstate, false);
+				window.addEventListener("hashchange", hashchange, false);
+			} else {
+				console.error("scrollr: unsupported browser");
+			}
+		};
+
+	init();
+
+})();
